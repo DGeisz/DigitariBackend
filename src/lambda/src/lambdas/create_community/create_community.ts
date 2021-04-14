e9da0470ad66a0440da8a4442f6d5672ca3608d3
Danny Geisz <dannygeisz@berkeley.edu>
2021-04-14 08:08:52 -0600

    Updated create community lambda, updated get am following community

--- src/lambda/src/lambdas/create_community/create_community.ts
+++ src/lambda/src/lambdas/create_community/create_community.ts
@@ -2,8 +2,6 @@ import { RdsClient } from "../../data_clients/rds_client/rds_client";
 import { DynamoDB } from "aws-sdk";
 import { Client } from "elasticsearch";
 import { AppSyncIdentityCognito, AppSyncResolverEvent } from "aws-lambda";
-import { FollowEventArgs } from "../../global_types/follow_event_args";
-import { FollowEntityActivity } from "../../global_types/FollowEntityType";
 import {
     DIGITARI_COMMUNITIES,
     DIGITARI_PRICES,
@@ -15,7 +13,6 @@ import { UserType } from "../../global_types/UserTypes";
 import { ranking2Tier } from "../../utils/tier_utils";
 import { v4 } from "uuid";
 import { insertFollowRow } from "../follow_user/rds_queries/queries";
-import { CommunityType } from "../../global_types/CommunityTypes";
 
 const rdsClient = new RdsClient();
 
@@ -145,20 +142,23 @@ export async function handler(event: AppSyncResolverEvent<EventArgs>) {
     /*
      * Index the community
      */
-    try {
-        await esClient.index({
-            index: "search",
-            type: "search_entity",
-            body: {
-                id: cid,
-                name: event.arguments.name,
-                followers: 1,
-                entityType: 1,
-            },
-        });
-    } catch (e) {
-        throw new Error("ES Error: " + e);
-    }
+    // try {
+    let doc = await esClient.index({
+        index: "search",
+        type: "search_entity",
+        id: cid,
+        body: {
+            id: cid,
+            name: event.arguments.name,
+            followers: 1,
+            entityType: 1,
+        },
+    });
+
+    // throw new Error("This is the indexed doc: " + JSON.stringify(doc));
+    // } catch (e) {
+    //     throw new Error("ES Error: " + e);
+    // }
 
     /*
      * Increment the source's following field, and decrease the
