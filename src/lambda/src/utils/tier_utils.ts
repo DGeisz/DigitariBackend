6d6e511d37d16f316241dc483fd5ab41a1604448
Danny Geisz <dannygeisz@berkeley.edu>
2021-06-09 21:37:21 -0600

    Fixed response cost/convo reward"

--- src/lambda/src/utils/tier_utils.ts
+++ src/lambda/src/utils/tier_utils.ts
@@ -5,19 +5,19 @@ export function tier2convoReward(tier: number): number {
         case -2:
             return 4;
         case -1:
-            return 15;
+            return 10;
         case 0:
-            return 30;
+            return 20;
         case 1:
-            return 45;
+            return 40;
         case 2:
-            return 75;
+            return 80;
         case 3:
-            return 100;
+            return 120;
         case 4:
-            return 150;
+            return 200;
         case 5:
-            return 250;
+            return 300;
         case 6:
             return 400;
         default:
@@ -36,17 +36,17 @@ export function tier2responseCost(tier: number): number {
         case 0:
             return 10;
         case 1:
-            return 15;
+            return 20;
         case 2:
-            return 25;
+            return 40;
         case 3:
-            return 35;
+            return 60;
         case 4:
-            return 50;
+            return 100;
         case 5:
-            return 85;
+            return 150;
         case 6:
-            return 135;
+            return 200;
         default:
             return 0;
     }
