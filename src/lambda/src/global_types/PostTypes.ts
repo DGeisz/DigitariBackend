6188591ecc295551077b7e257d0c3042f58e396b
Danny Geisz <dannygeisz@berkeley.edu>
2021-06-19 10:42:34 -0600

    Updated block post cost

--- src/lambda/src/global_types/PostTypes.ts
+++ src/lambda/src/global_types/PostTypes.ts
@@ -1,7 +1,7 @@
 export const POST_CONTENT_MAX_LEN = 250;
 export const POST_ADD_ON_CONTENT_MAX_LEN = 10000;
 
-export const POST_BLOCK_COST = 30;
+export const POST_BLOCK_COST = 200;
 
 export enum PostAddOn {
     None,
