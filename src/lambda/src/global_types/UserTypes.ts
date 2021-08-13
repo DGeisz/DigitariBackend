847ae082bcbe18ceb291dcfda5c5d395c97609d4
Danny Geisz <dannygeisz@berkeley.edu>
2021-08-13 09:06:44 -0600

    Updated upgrade wallet price

--- src/lambda/src/global_types/UserTypes.ts
+++ src/lambda/src/global_types/UserTypes.ts
@@ -132,21 +132,14 @@ const WALLET_BASE_SIZE = 100;
 const WALLET_PRICE_MULTIPLIER = 1.84;
 const WALLET_BASE_PRICE = 20;
 
+const WALLET_PRICE_COEFF = 1;
+
 /*
  * Calculate the price and size of the next wallet upgrade
  */
 export function calculateWalletUpgrade(maxWallet: number): [number, number] {
-    const currentExp = Math.ceil(
-        Math.log(maxWallet / WALLET_BASE_SIZE) / Math.log(1.6)
-    );
-
-    const nextPrice = makePrettyNumber(
-        WALLET_BASE_PRICE * WALLET_PRICE_MULTIPLIER ** currentExp
-    );
-
-    const nextSize = makePrettyNumber(
-        WALLET_BASE_SIZE * WALLET_MULTIPLIER ** (currentExp + 1)
-    );
+    const nextPrice = makePrettyNumber(maxWallet * WALLET_PRICE_COEFF);
+    const nextSize = makePrettyNumber(maxWallet * WALLET_MULTIPLIER);
 
     return [nextPrice, nextSize];
 }
